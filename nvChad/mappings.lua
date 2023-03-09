7eb0fe36c761f87984a62beaf5c5b719d0ffc617
Julian Archila <julianarchilacaro@gmail.com>
2023-03-09 17:13:09 -0500

    nvchad config

--- nvChad/mappings.lua
+++ nvChad/mappings.lua
@@ -1,51 +1,117 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-  -- Navigate buffers
-    ["<S-l>"] = {"<cmd> bnext <CR>", "switch to next buffer"},
-    ["<S-h>"] = {"<cmd> bprevious<CR>", "switch to previous buffer"},
-    ["<leader>c"] = {"<cmd> bd<CR>", "Close current buffer"},
+		-- Shortcut to open a file in a new tab
+		["<leader>tt"] = { "<cmd> tabnew <CR>", "open a new tab" },
+		["<leader>tn"] = { "<cmd> tabnew <CR>", "open a new tab" },
+		["<leader>to"] = { "<cmd> tabonly <CR>", "close all other tabs" },
+		["<leader>tc"] = { "<cmd> tabclose <CR>", "close current tab" },
+		["<leader>tm"] = { "<cmd> tabmove <CR>", "move current tab" },
+		["<leader>tr"] = { "<cmd> tabnext <CR>", "move to next tab" },
+		["<leader>tl"] = { "<cmd> tabprevious <CR>", "move to previous tab" },
+		["<leader>th"] = { "<cmd> tabfirst <CR>", "move to first tab" },
+		["<leader>te"] = { "<cmd> tablast <CR>", "move to last tab" },
+		["<leader>ts"] = { "<cmd> tabsplit <CR>", "split current tab" },
+		["<leader>tv"] = { "<cmd> vsplit <CR>", "split current tab vertically" },
 
-  -- Shortcut to open a file in a new tab
-  ["<leader>tt"] = { "<cmd> tabnew <CR>", "open a new tab" },
-  ["<leader>tn"] = { "<cmd> tabnew <CR>", "open a new tab" },
-  ["<leader>to"] = { "<cmd> tabonly <CR>", "close all other tabs" },
-  ["<leader>tc"] = { "<cmd> tabclose <CR>", "close current tab" },
-  ["<leader>tm"] = { "<cmd> tabmove <CR>", "move current tab" },
-  ["<leader>tr"] = { "<cmd> tabnext <CR>", "move to next tab" },
-  ["<leader>tl"] = { "<cmd> tabprevious <CR>", "move to previous tab" },
-  ["<leader>th"] = { "<cmd> tabfirst <CR>", "move to first tab" },
-  ["<leader>te"] = { "<cmd> tablast <CR>", "move to last tab" },
-  ["<leader>ts"] = { "<cmd> tabsplit <CR>", "split current tab" },
-  ["<leader>tv"] = { "<cmd> vsplit <CR>", "split current tab vertically" },
+		-- Window management
+		["<leader>w"] = { "<cmd> w <CR>", "save current file" },
+		["<leader>q"] = { "<cmd> q <CR>", "close current file" },
+		["<leader>wq"] = { "<cmd> wq <CR>", "save and close current file" },
 
+		-- Resize with arrows
 
+		["<C-Up>"] = { "<cmd> resize +2 <CR>", "resize +2" },
+		["<C-Down>"] = { "<cmd> resize -2 <CR>", "resize -2" },
+		["<C-Left>"] = { "<cmd> vertical resize -2 <CR>", "vertical resize -2" },
+		["<C-Right>"] = { "<cmd> vertical resize +2 <CR>", "vertical resize +2" },
+	},
+}
 
+M.tabufline = {
+	plugin = true,
 
-  -- Window management 
-  ["<leader>w"] = { "<cmd> w <CR>", "save current file" },
-  ["<leader>q"] = { "<cmd> q <CR>", "close current file" },
-  ["<leader>wq"] = { "<cmd> wq <CR>", "save and close current file" },
+	n = {
+		-- Navigate buffers
+		["<S-h>"] = {
+			function()
+				require("nvchad_ui.tabufline").tabuflinePrev()
+			end,
+			"goto prev buffer",
+		},
+		["<S-l>"] = {
+			function()
+				require("nvchad_ui.tabufline").tabuflineNext()
+			end,
+			"goto next buffer",
+		},
 
+		["<leader>c"] = {
+			function()
+				require("nvchad_ui.tabufline").close_buffer()
+			end,
+			"close buffer",
+		},
+	},
+}
 
--- Resize with arrows
--- keymap("n", "<C-Up>", ":resize +2<CR>", opts)
--- keymap("n", "<C-Down>", ":resize -2<CR>", opts)
--- keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
--- keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-  ["<C-Up>"] = {"<cmd> resize +2 <CR>", "resize +2"},
-  ["<C-Down>"] = {"<cmd> resize -2 <CR>", "resize -2"},
-  ["<C-Left>"] = {"<cmd> vertical resize -2 <CR>", "vertical resize -2"},
-  ["<C-Right>"] = {"<cmd> vertical resize +2 <CR>", "vertical resize +2"},
+M.lsp = {
 
-  },
+	n = {
+		["<leader>k"] = {
+			function()
+				vim.diagnostic.setloclist()
+			end,
+			"diagnostic setloclist",
+		},
+		["<leader>i"] = {
+			function()
+				vim.diagnostic.open_float()
+			end,
+			"floating diagnostic",
+		},
+		["<leader>f"] = {
+			function()
+				vim.lsp.buf.format({ async = true })
+			end,
+			"lsp formatting",
+		},
+	},
+}
 
+M.comment = {
+	plugin = true,
 
+	-- toggle comment in both modes
+	n = {
+		["<C-_>"] = {
+			function()
+				require("Comment.api").toggle.linewise.current()
+			end,
+			"toggle comment",
+		},
+	},
+
+	v = {
+		["<C-_>"] = {
+			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+			"toggle comment",
+		},
+	},
 }
 
 -- more keybinds!
+M.telescope = {
+	plugin = true,
+
+	n = {
+		-- find
+		["<C-p>"] = { "<cmd> Telescope find_files <CR>", "find files" },
+		["<C-P>"] = { "<cmd> Telescope buffers <CR>", "find buffers" },
+	},
+}
 
 return M
--- nvChad/plugins/init.lua
+++ nvChad/plugins/init.lua
@@ -1,36 +1,60 @@
-local overrides = require "custom.plugins.overrides"
+local overrides = require("custom.plugins.overrides")
+
+print("checkpoint")
 
 local plugins = {
 
-  ["zbirenbaum/copilot.lua"] = {
-      config = function()
-         vim.defer_fn(function()
-            require("copilot").setup {
-               cmp = {
-                  enabled = true,
-                  method = "getCompletionsCycling",
-               },
-            }
-         end, 100)
-      end,
-   },
-   ["zbirenbaum/copilot-cmp"] = {},
+	["zbirenbaum/copilot.lua"] = {
+		event = { "VimEnter" },
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup({})
+			end, 100)
+		end,
+	},
+	["zbirenbaum/copilot-cmp"] = {
+		after = {
+			"copilot.lua",
+			"nvim-cmp",
+		},
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 
-  --CMP
-  ["hrsh7th/nvim-cmp"] = {
-    override_options = overrides.cmp,
-  },
- -- overrde plugin configs
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = overrides.treesitter,
-  },
+	--CMP
+	["hrsh7th/nvim-cmp"] = {
+		override_options = overrides.cmp,
+	},
+	-- overrde plugin configs
+	["nvim-treesitter/nvim-treesitter"] = {
+		override_options = overrides.treesitter,
+	},
+	["williamboman/mason.nvim"] = {
+		override_options = overrides.mason,
+	},
 
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-},
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
+
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
+
+	["NvChad/ui"] = {
+		override_options = {
+			tabufline = {
+				lazyload = false, -- to show tabufline by default
+			},
+		},
+	},
 }
 
 return plugins
