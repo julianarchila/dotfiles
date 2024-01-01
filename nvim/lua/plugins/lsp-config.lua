2510f49754355e7cb550e58afc39ce2e69e411cf
Julian Archila <julianarchilacaro@gmail.com>
2024-01-01 12:53:10 -0500

    Neovim: minor

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,49 +1,52 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ensure_installed = {
-					"tailwindcss-language-server",
-					"prettierd",
-					"eslint_d",
-					"lua-language-server",
-					"typescript-language-server",
-					"stylua",
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ensure_installed = {
+          "tailwindcss-language-server",
+          "prettierd",
+          "eslint_d",
+          "lua-language-server",
+          "typescript-language-server",
+          "stylua",
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			local servers = { "tsserver", "html", "lua_ls", "tailwindcss" }
-			for _, lsp in ipairs(servers) do
-				lspconfig[lsp].setup({
-					capabilites = capabilities,
-				})
-			end
+      local servers = { "tsserver", "html", "lua_ls", "tailwindcss" }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+          capabilites = capabilities,
+        })
+      end
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
+    end,
+  },
 }
