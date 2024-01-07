828ab41114e57828ed32dd8a37ffb6eb6125c526
Julian Archila <julianarchilacaro@gmail.com>
2024-01-07 18:21:05 -0500

    Neovim: move mason-null-ls config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -16,19 +16,6 @@ return {
 		end,
 	},
 	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = { "stylua", "prettierd", "eslint_d", "clang_format" },
-			})
-		end,
-	},
-	{
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
 		opts = {
