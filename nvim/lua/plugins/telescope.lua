4a1f7c404c5d835b69e01cc9406a28e5fdc97d6c
Julian Archila <julianarchilacaro@gmail.com>
2024-02-14 17:58:51 -0500

    Nvim: disable bufferline, telescope keymap for buffers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
 			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<C-b>", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
 			require("telescope").load_extension("ui-select")
