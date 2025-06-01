6709eb15ca63a9b0dff64998f5c2cd85fbbbda89
Julian Archila <julianarchilacaro@gmail.com>
2025-06-01 02:25:41 -0500

    update nvim and zed theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -3,9 +3,18 @@ return {
     "sainnhe/gruvbox-material",
 
     config = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_foreground = "mix"
       vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_enable_bold = 1
-      -- vim.cmd("colorscheme gruvbox-material")
+      vim.g.gruvbox_material_ui_contrast = "high"
+      vim.g.gruvbox_material_float_style = "bright"
+      vim.g.gruvbox_material_statusline_style = "material"
+      vim.g.gruvbox_material_cursor = "auto"
+
+      -- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
+      vim.g.gruvbox_material_better_performance = 1
+
+      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
   {
@@ -37,7 +46,7 @@ return {
         filter = "classic",
         transparent_background = true,
       })
-      vim.cmd("colorscheme monokai-pro")
+      -- vim.cmd("colorscheme monokai-pro")
     end,
   },
   {
