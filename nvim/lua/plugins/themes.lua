b14d853b3477cb4ff0d385efd2d5fde63c027143
Julian Archila <julianarchilacaro@gmail.com>
2025-06-01 19:45:11 -0500

    Nvim: gruvbox transparent background

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -11,7 +11,7 @@ return {
       vim.g.gruvbox_material_statusline_style = "material"
       vim.g.gruvbox_material_cursor = "auto"
 
-      -- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
+      vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
       vim.g.gruvbox_material_better_performance = 1
 
       vim.cmd.colorscheme("gruvbox-material")
